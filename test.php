f148e687d6c5253728af4f38e55e4cf9f01844ac
Aditzz7 <aditzz7@gmail.com>
2025-11-14 21:48:53 +0800

    Remove unused test and old index files

--- test.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo "PHP is working!";
-echo "<br>Current directory: " . __DIR__;
-echo "<br>Server: " . $_SERVER['SERVER_NAME'];
-echo "<br>Request URI: " . $_SERVER['REQUEST_URI'];
-echo "<br>Document Root: " . $_SERVER['DOCUMENT_ROOT'];
-?>
